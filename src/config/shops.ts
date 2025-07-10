9ed397f07b6440c9fc18747454e41b0e794801a8
karthikeyan-133 <135159837+karthikeyan-133@users.noreply.github.com>
2025-07-10 16:54:11 +0530

    Update shops.ts

--- src/config/shops.ts
+++ src/config/shops.ts
@@ -9,24 +9,81 @@ export interface Shop {
 export const SHOPS: Shop[] = [
   {
     id: 'shop-1',
-    name: 'Zyra',
+    name: 'Al Ameen',
     isActive: true
   },
   {
     id: 'shop-2',
-    name: 'KL 70',
+    name: 'Al Cafe',
     isActive: true
   },
   {
     id: 'shop-3',
-    name: 'AL Cafe',
+    name: 'AMR',
     isActive: true
   },
   {
     id: 'shop-4',
-    name: 'Metro Plaza',
+    name: 'Fish Shop',
     isActive: true
-  }
+  },
+  {
+    id: 'shop-5',
+    name: 'Food Dude',
+    isActive: true
+  },
+  {
+    id: 'shop-6',
+    name: 'Haritham Vegetables',
+    isActive: true
+  },
+  {
+    id: 'shop-7',
+    name: 'KL 70',
+    isActive: true
+  },
+  {
+    id: 'shop-8',
+    name: 'Malabar Green',
+    isActive: true
+  },
+  {
+    id: 'shop-9',
+    name: 'Metro Bakery',
+    isActive: true
+  },
+  {
+    id: 'shop-10',
+    name: 'Milko',
+    isActive: true
+  },
+  {
+    id: 'shop-11',
+    name: 'Moon Star',
+    isActive: true
+  },
+  {
+    id: 'shop-12',
+    name: 'Muji Biriyani',
+    isActive: true
+  },
+  {
+    id: 'shop-13',
+    name: 'Nazar',
+    isActive: true
+  },
+  {
+    id: 'shop-14',
+    name: 'Thejus Bakery',
+    isActive: true
+  },
+  {
+    id: 'shop-15',
+    name: 'Zyra',
+    isActive: true
+  },
+  
+  
 ];
 
 export const getActiveShops = (): Shop[] => {
